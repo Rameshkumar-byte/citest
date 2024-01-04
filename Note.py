107e416d384f2400e99dcac09d7333523243c419
Rameshkumar-byte <154999535+Rameshkumar-byte@users.noreply.github.com>
2024-01-04 14:59:59 +0530

    Update Note.py

--- Note.py
+++ Note.py
@@ -1,10 +1,7 @@
 import subprocess
-
-# Specify the path to your batch file
 batch_file_path = r'E:\note.bat'
 
 try:
-    # Run the batch file using subprocess
     subprocess.run([batch_file_path], shell=True, check=True)
 except subprocess.CalledProcessError as e:
     print(f"Error executing batch file: {e}")
