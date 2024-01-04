7b8e33fa492d3301a252cf3afc81d4c9653b27b6
Rameshkumar-byte <154999535+Rameshkumar-byte@users.noreply.github.com>
2024-01-04 15:27:55 +0530

    Create Note_Jenkins.py

---
+++ Note_Jenkins.py
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Example Stage') {
+            steps {
+                script {
+                    try {
+                        bat 'E:\\note.bat'
+                    } catch (Exception e) {
+                        echo "Error executing batch file: ${e.message}"
+                        currentBuild.result = 'FAILURE'
+                        error("Error executing batch file")
+                    }
+                }
+            }
+        }
+    }
+}
